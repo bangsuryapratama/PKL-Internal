a3a440195ff2cc614441765b1b4e1392cf764828
SuryaPratama-Surz <manjirotama@gmail.com>
2025-06-24 15:54:00 +0700

    user

--- routes/web.php
+++ routes/web.php
@@ -3,60 +3,69 @@
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\ProductController;
 use App\Http\Controllers\BackendController;
+use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MyController;
 use App\Http\Middleware\Admin;
 use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-//basic route 
-Route::get('/tentang', function () {
-    return '<h1>Selamat Datang di Surya E-Commerce</h1>
-            <p>Ini adalah halaman tentang kami.</p>';
-});
+// Route::get('/', function () {
+//     return view('layouts.frontend');
+// });
 
-//profile route
-Route::get('/profile', function () {
-    return view('profile');
-});
+// //basic route 
+// Route::get('/tentang', function () {
+//     return '<h1>Selamat Datang di Surya E-Commerce</h1>
+//             <p>Ini adalah halaman tentang kami.</p>';
+// });
 
-//param 
-Route::get('/produk/{p}', function ($p) {
-    return "Saya membeli: " . $p;
-});
+// //profile route
+// Route::get('/profile', function () {
+//     return view('profile');
+// });
 
-//route dengan parameter kategori
-Route::get('/kategori/{namaKategori}', function ($namaKategori) {
-    return view ('kategori', compact('namaKategori'));
-    // return view ('kategori', ['kat' => $namaKategori]);
-});
+// //param 
+// Route::get('/produk/{p}', function ($p) {
+//     return "Saya membeli: " . $p;
+// });
 
-//optional parameter
-Route::get('/search/{keyword?}', function ($key = null) {
-    return view('search', compact('key'));
-});
+// //route dengan parameter kategori
+// Route::get('/kategori/{namaKategori}', function ($namaKategori) {
+//     return view ('kategori', compact('namaKategori'));
+//     // return view ('kategori', ['kat' => $namaKategori]);
+// });
 
-//latihan route barang dan kode 
-Route::get('/promo/{barang?}/{kode?}', function ($barang = null, $kode = null ) {
-    return view('promo', compact('barang', 'kode'));
-});
+// //optional parameter
+// Route::get('/search/{keyword?}', function ($key = null) {
+//     return view('search', compact('key'));
+// });
 
-//route untuk controller MyController
-Route::get('buku', [MyController::class, 'index'])->name('buku.index');
-//create buku
-Route::get('buku/create', [MyController::class, 'create'])->name('buku.create');
-Route::post('buku', [MyController::class, 'store']);
-//show buku
-Route::get('buku/{id}', [MyController::class, 'show'])->name('buku.show');
-//edit buku
-Route::get('buku/{id}/edit', [MyController::class, 'edit'])->name('buku.edit');
-Route::put('buku/{id}', [MyController::class, 'update'])->name('buku.update');
-//destroy
-Route::delete('buku/{id}', [MyController::class, 'destroy']);
+// //latihan route barang dan kode 
+// Route::get('/promo/{barang?}/{kode?}', function ($barang = null, $kode = null ) {
+//     return view('promo', compact('barang', 'kode'));
+// });
+
+// //route untuk controller MyController
+// Route::get('buku', [MyController::class, 'index'])->name('buku.index');
+// //create buku
+// Route::get('buku/create', [MyController::class, 'create'])->name('buku.create');
+// Route::post('buku', [MyController::class, 'store']);
+// //show buku
+// Route::get('buku/{id}', [MyController::class, 'show'])->name('buku.show');
+// //edit buku
+// Route::get('buku/{id}/edit', [MyController::class, 'edit'])->name('buku.edit');
+// Route::put('buku/{id}', [MyController::class, 'update'])->name('buku.update');
+// //destroy
+// Route::delete('buku/{id}', [MyController::class, 'destroy']);
+
 Auth::routes();
+
+//Route tamu atau member
+Route::get('/', [FrontendController::class,'index']);
+Route::get('/product', [FrontendController::class, 'product']);
+Route::get('/product/{product}', [FrontendController::class , 'singleProduct']);
+Route::get('/about', [FrontendController::class, 'about'])->name('about'); 
+Route::get('/cart', [FrontendController::class, 'cart']); 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
